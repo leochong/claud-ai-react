6e2d8c6abf681f359b606d64c1e50bc4661aae7f
Chongolnee <roongrunchai.chongolnee@philips.com>
2025-06-04 08:00:22 -0700

    trying to fix S3 bucket issue

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -2,6 +2,9 @@ import { defineStorage } from '@aws-amplify/backend-storage';
 
 export const storage = defineStorage({
   name: 'UserFiles',
+  bucket: {
+    name: 'user-files-storage-bucket',
+  },
   permissions: {
     authenticated: {
       access: ['read', 'write'],
